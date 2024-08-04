5c480feee7840effe07a413a3aaf057666d2cbbd
Ruslan <rmir212121@gmail.com>
2024-08-04 15:47:39 +0400

    Added navigation
    and also secrets files

--- index.ts
+++ index.ts
@@ -5,6 +5,36 @@ const puppeteer = require("puppeteer");
 const SS_FOLDER: string = "./screenshots/";
 const url = "https://wallet.coinbase.com/ocs/progress";
 
+// enter site
+const welcomeCloseButton = 'button[data-testid="nux-welcome-modal-close"]';
+const walletSelectButton =
+  'button[data-testid="coinbase-wallet-option-cell-pressable"]';
+const mobileSelectButton =
+  'button[data-testid="coinbase-wallet-selector-wallet-cell-pressable"]';
+
+// in extention window select import wallet
+const importExistingButton = 'button[data-testid="btn-import-existing-wallet"]';
+const pkButton = 'button[data-testid="btn-import-recovery-phrase"]';
+
+// enter private key and ack sman alert
+const scamAlertAckButtonXpath =
+  'xpath=//*[@id="modalsContainer"]/div/div/div[2]/div/div/div/div[2]/button';
+const pkInputXpath =
+  'xpath=//*[@id="modalsContainer"]/div/div/div[2]/div/div/div/div[2]/button';
+const submitPKButton = 'button[data-testid="btn-import-wallet"]';
+
+// create password
+const inputPWXpath = 'xpath=//*[@id="cds-textinput-label-:rh:"]';
+const inputPWAgainXpath = 'xpath=//*[@id="cds-textinput-label-:ri:"]';
+const agreeCBoxXpath = 'xpath=//*[@id="collapsible-:rj:"]';
+const submitButton = 'button[data-testid="btn-password-continue"]';
+
+//connect auth btn
+const connectButton = 'button[data-testid="allow-authorize-button"]';
+
+const spinSelectButton = 'button[data-testid="spinwheelButton"]';
+const spin2win = 'button[data-testid="spinWheelButton"]';
+
 // const extention_id_string = "hnfanknocfeofbddgcijnmhnfnkdnaad";
 // const extention_version =
 
@@ -12,27 +42,10 @@ function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-const navigate = async (page: Page) => {
-  const [gettngStartedClose] = await Promise.all([
-    page.waitForSelector('button[data-testid="nux-welcome-modal-close"]'),
-    page.click('button[data-testid="nux-welcome-modal-close"]'),
-  ]);
-
-  const [selectingWallet] = await Promise.all([
-    page.waitForSelector(
-      'button[data-testid="coinbase-wallet-option-cell-pressable"]',
-    ),
-    page.click('button[data-testid="coinbase-wallet-option-cell-pressable"]'),
-  ]);
-
-  const [selectMobile] = await Promise.all([
-    page.waitForSelector(
-      'button[data-testid="coinbase-wallet-selector-wallet-cell-pressable"]',
-    ),
-    page.click(
-      'button[data-testid="coinbase-wallet-selector-wallet-cell-pressable"]',
-    ),
-  ]);
+const navigate_to_login = async (page: Page) => {
+  await page.click(welcomeCloseButton);
+  await page.click(walletSelectButton);
+  await page.click(mobileSelectButton);
 };
 
 const main = async () => {
@@ -48,7 +61,7 @@ const main = async () => {
   await page.goto(url, { waitUntil: "load" });
   await page.setViewport({ width: 1080, height: 1024 });
 
-  // await navigate(page);
+  await navigate_to_login(page);
 
   // await page.screenshot({ path: `screenshots/screen.png` });
 
