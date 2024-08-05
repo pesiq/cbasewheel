3960bd9bfc456356d94dcd2f598c2deb047b7cb3
Ruslan <rmir212121@gmail.com>
2024-08-05 17:53:05 +0400

    Finished wallet registration
    Need to handle auth

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
+import { exit } from "process";
 import { Browser, Page } from "puppeteer";
-
-const puppeteer = require("puppeteer");
+import * as fs from "fs";
+import * as rd from "readline";
+import puppeteer from "puppeteer";
+// const puppeteer = require("puppeteer");
 
 const SS_FOLDER: string = "./screenshots/";
 const url = "https://wallet.coinbase.com/ocs/progress";
@@ -19,14 +22,13 @@ const pkButton = 'button[data-testid="btn-import-recovery-phrase"]';
 // enter private key and ack sman alert
 const scamAlertAckButtonXpath =
   'xpath=//*[@id="modalsContainer"]/div/div/div[2]/div/div/div/div[2]/button';
-const pkInputXpath =
-  'xpath=//*[@id="modalsContainer"]/div/div/div[2]/div/div/div/div[2]/button';
+const pkInput = 'input[aria-label="Recovery phrase or private key"]';
 const submitPKButton = 'button[data-testid="btn-import-wallet"]';
 
 // create password
-const inputPWXpath = 'xpath=//*[@id="cds-textinput-label-:rh:"]';
-const inputPWAgainXpath = 'xpath=//*[@id="cds-textinput-label-:ri:"]';
-const agreeCBoxXpath = 'xpath=//*[@id="collapsible-:rj:"]';
+const passwordInput = 'input[aria-label="Password"]';
+const verifyPasswordInput = 'input[aria-label="Verify password"]';
+const privacyButton = 'input[data-testid="terms-and-privacy-policy"]';
 const submitButton = 'button[data-testid="btn-password-continue"]';
 
 //connect auth btn
@@ -34,6 +36,9 @@ const connectButton = 'button[data-testid="allow-authorize-button"]';
 
 const spinSelectButton = 'button[data-testid="spinwheelButton"]';
 const spin2win = 'button[data-testid="spinWheelButton"]';
+
+const connectRainbowButton =
+  'button[data-testid="gradient-connect-wallet-btn"]';
 
 // const extention_id_string = "hnfanknocfeofbddgcijnmhnfnkdnaad";
 // const extention_version =
@@ -48,22 +53,55 @@ const navigate_to_login = async (page: Page) => {
   await page.click(mobileSelectButton);
 };
 
+const login_into_coinbase = async (page: Page, privateKey: string) => {
+  await page.click(importExistingButton);
+  await page.click(pkButton);
+  await page.click(scamAlertAckButtonXpath);
+
+  // input private key
+  await page.type(pkInput, privateKey);
+  await page.click(submitPKButton);
+  await delay(2000);
+  // create PW
+  const pw = "balls";
+  await page.type(passwordInput, pw);
+  await page.type(verifyPasswordInput, pw);
+  await page.click(privacyButton);
+  await page.click(submitButton);
+
+  await delay(2000);
+  // allow extention to work
+  await page.click(connectButton);
+};
+
+const handleNewPage = async (target: any) => {
+  if (target.type() === "page") {
+    const newPage = await target.page();
+    await newPage.waitForSelector("body"); // Ensure the new page has loaded
+    console.log("New page opened:", await newPage.title());
+
+    login_into_coinbase(newPage, "benis");
+  }
+};
+
 const main = async () => {
   const browser: Browser = await puppeteer.launch({
     headless: false,
-    slowMo: 250,
+    slowMo: 50,
     args: [
       "--disable-extensions-except=./extentions/wallet/",
       "--load-extension=./extentions/wallet/",
     ],
   });
   const page: Page = await browser.newPage();
+  browser.on("targetcreated", handleNewPage);
   await page.goto(url, { waitUntil: "load" });
   await page.setViewport({ width: 1080, height: 1024 });
-
+  await delay(2000);
   await navigate_to_login(page);
+  console.log("Navigated to extention");
 
-  // await page.screenshot({ path: `screenshots/screen.png` });
+  await page.click(connectRainbowButton);
 
   // await browser.close();
 };
